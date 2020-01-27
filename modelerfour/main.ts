826129f3378c7850ed2411b5b052ed53408603e9
Garrett Serack <garretts@microsoft.com>
2020-01-27 13:17:24 -0800

    enable grouper

--- modelerfour/main.ts
+++ modelerfour/main.ts
@@ -2,9 +2,11 @@ import { AutoRestExtension, } from '@azure-tools/autorest-extension-base';
 import { processRequest as modelerfour } from './modeler/plugin-modelerfour';
 import { processRequest as preNamer } from './prenamer/plugin-prenamer';
 import { processRequest as flattener } from './flattener/plugin-flattener';
+import { processRequest as grouper } from './grouper/plugin-grouper';
 
 export async function initializePlugins(pluginHost: AutoRestExtension) {
   pluginHost.Add('modelerfour', modelerfour);
+  pluginHost.Add('grouper', grouper);
   pluginHost.Add('pre-namer', preNamer);
   pluginHost.Add('flattener', flattener);
 }
