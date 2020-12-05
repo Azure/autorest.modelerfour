c6cd4247e30975d91ac609550b1f6aba620903c6
Timothee Guerin <tiguerin@microsoft.com>
2020-12-04 16:00:56 -0800

    Add back license header

--- modelerfour/testv2/modeler/modelerfour.test.ts
+++ modelerfour/testv2/modeler/modelerfour.test.ts
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 import { ModelerFour } from "../../modeler/modelerfour";
 import { CodeModel, Parameter, SchemaResponse, ConstantSchema, SealedChoiceSchema } from "@azure-tools/codemodel";
 import { ModelerFourOptions } from "../../modeler/modelerfour-options";
