20ca12f012fba6515c639fc7c93e7f27acf08fd0
Timothee Guerin <tiguerin@microsoft.com>
2021-01-25 11:59:46 -0800

    Respect required attribute for request body (#385)

--- modelerfour/test/modeler/modelerfour.test.ts
+++ modelerfour/test/modeler/modelerfour.test.ts
@@ -2,51 +2,16 @@
  *  Copyright (c) Microsoft Corporation. All rights reserved.
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
-import { ModelerFour } from "../../src/modeler/modelerfour";
-import { CodeModel, Parameter, SchemaResponse, ConstantSchema, SealedChoiceSchema } from "@azure-tools/codemodel";
-import { ModelerFourOptions } from "../../src/modeler/modelerfour-options";
+import { Parameter, SchemaResponse, ConstantSchema, SealedChoiceSchema } from "@azure-tools/codemodel";
 import {
   addOperation,
   addSchema,
-  createTestSessionFromModel,
   createTestSpec,
   InitialTestSpec,
   response,
   responses,
 } from "../utils";
-import { Model } from "@azure-tools/openapi";
-
-const modelerfourOptions: ModelerFourOptions = {
-  "flatten-models": true,
-  "flatten-payloads": true,
-  "group-parameters": true,
-  "resolve-schema-name-collisons": true,
-  "additional-checks": true,
-  "always-create-accept-parameter": true,
-  //'always-create-content-type-parameter': true,
-  "naming": {
-    override: {
-      $host: "$host",
-      cmyk: "CMYK",
-    },
-    local: "_ + camel",
-    constantParameter: "pascal",
-  },
-};
-
-const cfg = {
-  "modelerfour": modelerfourOptions,
-  "payload-flattening-threshold": 2,
-};
-
-async function runModeler(spec: any, config: { modelerfour: ModelerFourOptions } = cfg): Promise<CodeModel> {
-  const { session, errors } = await createTestSessionFromModel<Model>(config, spec);
-  const modeler = await new ModelerFour(session).init();
-
-  expect(errors.length).toBe(0);
-
-  return modeler.process();
-}
+import { runModeler } from "./modelerfour-utils";
 
 export function findByName<T>(name: string, items: Array<T> | undefined): T | undefined {
   return (items && items.find((i) => (<any>i).language.default.name === name)) || undefined;
